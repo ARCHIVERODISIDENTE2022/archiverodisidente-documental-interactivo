63160d93086c226347388f7955e8b54de190d8a6
Constanza <c.leytonleiva@gmail.com>
2022-12-19 13:05:21 -0300

    div partticipantes

--- src/routes/VistaParticipantes.jsx
+++ src/routes/VistaParticipantes.jsx
@@ -1,46 +1,64 @@
-import { React, useContext, useEffect, useState } from "react";
+import { React, useEffect, useState } from "react";
 import { ListaParticipantes } from "../components/ListaParticipantes";
 //import { contextParticipantes } from "../context/DataProvider";
 import axios from "axios";
 import Footer from "../components/Footer";
-import "./VistaParticipantes.css"
 
 const url =
-    "https://raw.githubusercontent.com/Cleytonleiva/archiverodata/main/archiveroMock.json";
+  "https://raw.githubusercontent.com/Cleytonleiva/archiverodata/main/archiveroMock.json";
 
 export const VistaParticipantes = () => {
-    const [participantes, setParticipantes] = useState(null);
+  //const context = useContext(contextParticipantes);
+  const [participantes, setParticipantes] = useState(null);
 
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const response = await axios.get(url);
-                setParticipantes(response.data.participantes);
-            } catch (error) {
-                // Manejar el error acá
-            }
-        }
-
-        fetchData();
-    }, []);
-
-    if (participantes === null) {
-        return <div>Cargando...</div>;
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get(url);
+        setParticipantes(response.data.participantes);
+      } catch (error) {
+        // Manejar el error acá
+      }
     }
 
-    return (
-        <div className="vistaParticipantes">
-               <a className="backArrow" href="#choice">
-                        <i className="fa-solid fa-left-long"></i>
-                    </a>
-                            <h1 className="title">PARTICIPANTES</h1>
-            {participantes.map((participante) => {
-                return <>
-                            <ListaParticipantes participante={participante} />
-                        
-                    </>
-      })}
+    fetchData();
+  }, []);
+
+  if (participantes === null) {
+    return <div>Cargando...</div>;
+  }
+
+  return (
+    <div>
+      <a href="#choice" />
+      <div>
+        <h1> PARTICIPANTES </h1>
+      </div>
+      <div className="participantes">
+        {participantes.map((participante) => {
+          return (
+            <>
+              <div>
+                <ListaParticipantes participante={participante} />
+              </div>
+            </>
+          );
+        })}
+      </div>
+
       <Footer />
-    </div >
+    </div>
   );
 };
+
+// import { Link } from "react-router-dom";
+// import Footer from "../components/Footer";
+
+// const VistaParticipantes = () => {
+
+//     return (
+//
+//     )
+// }
+
+// export default VistaParticipantes
