c9c602dd3af300322ad58bff2f39e3b05f2a8dc9
Constanza <c.leytonleiva@gmail.com>
2023-01-26 18:50:53 -0300

    retribucion

--- src/routes/Retribucion.jsx
+++ src/routes/Retribucion.jsx
@@ -1,26 +1,64 @@
 import Footer from "../components/Footer";
-import "./Retribucion.css"
+import "./Retribucion.css";
 
 const Retribucion = () => {
-    return(
-        <>
+  return (
+    <>
+      <div className="containerRetribucion">
         <div className="retribucion">
-            <h1 className="titleRetribucion">RETRIBUCIÓN</h1>
-            <h2 className="descripcion">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem.</h2>
-            <div className="containerlinks">
-                <h3 className="name">www.todomejora.org</h3>
-                <h3 className="descripcionLink">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</h3>
-                <h3 className="name">www.todomejora.org</h3>
-                <h3 className="descripcionLink">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</h3>
-                <h3 className="name">www.todomejora.org</h3>
-                <h3 className="descripcionLink">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</h3>
-                <h3 className="name">www.todomejora.org</h3>
-                <h3 className="descripcionLink">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</h3>
+          <h1 className="titleRetribucion">RETRIBUCIÓN</h1>
+          <h2 className="descripcion">
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration in some form, by injected
+            humour, or randomised words which don't look even slightly
+            believable. If you are going to use a passage of Lorem Ipsum, you
+            need to be sure there isn't anything embarrassing hidden in the
+            middle of text. All the Lorem.
+          </h2>
+          <div className="containerlinks">
+            <div className="link">
+              <h3 className="name">www.todomejora.org</h3>
+              <h3 className="descripcionLink">
+                There are many variations of passages of Lorem Ipsum available,
+                but the majority have suffered alteration in some form, by
+                injected humour, or randomised words which don't look even
+                slightly believable.
+              </h3>
             </div>
-            <Footer/>
+            <div className="link">
+              <h3 className="name">www.todomejora.org</h3>
+              <h3 className="descripcionLink">
+                There are many variations of passages of Lorem Ipsum available,
+                but the majority have suffered alteration in some form, by
+                injected humour, or randomised words which don't look even
+                slightly believable.
+              </h3>
             </div>
-        </>
-    )
+            <div className="link">
+              <h3 className="name">www.todomejora.org</h3>
+              <h3 className="descripcionLink">
+                There are many variations of passages of Lorem Ipsum available,
+                but the majority have suffered alteration in some form, by
+                injected humour, or randomised words which don't look even
+                slightly believable.
+              </h3>
+            </div>
+            <div className="link">
+              <h3 className="name">www.todomejora.org</h3>
+              <h3 className="descripcionLink">
+                There are many variations of passages of Lorem Ipsum available,
+                but the majority have suffered alteration in some form, by
+                injected humour, or randomised words which don't look even
+                slightly believable.
+              </h3>
+            </div>
+          </div>
+        </div>
+
+        <Footer />
+      </div>
+    </>
+  );
 };
 
-export default Retribucion
+export default Retribucion;
