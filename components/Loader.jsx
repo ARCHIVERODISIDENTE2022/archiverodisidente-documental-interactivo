a652e1c7aa4c0a73a15964be331071a26f2df372
Estefanía Romero-Cors <zotapianola@gmail.com>
2023-06-16 17:03:47 -0400

    eslint 2 spaces fix

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -1,9 +1,9 @@
 import './Loader.css'
 
 export default function Loader () {
-    return (
-        <div className='loader-container'>
-            <div className='spinner' />
-        </div>
-    )
+  return (
+    <div className='loader-container'>
+      <div className='spinner' />
+    </div>
+  )
 }
