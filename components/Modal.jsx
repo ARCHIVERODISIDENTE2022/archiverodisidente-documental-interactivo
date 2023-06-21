26fb47cd5e193dca27debf5ef2d13b73e4323cd6
Zota <zotapianola@gmail.com>
2023-06-21 18:24:54 -0400

    modal sin estilos

---
+++ components/Modal.jsx
@@ -0,0 +1,17 @@
+import ReactModal from 'react-modal'
+import './Modal.css'
+
+const Modal = ({ isOpen, onRequestClose, children }) => {
+  return (
+    <ReactModal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      contentLabel='Modal'
+      ariaHideApp={false}
+    >
+      {children}
+    </ReactModal>
+  )
+}
+
+export default Modal
