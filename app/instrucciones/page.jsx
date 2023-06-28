663d49d757e4626929b01658a8470b23f23cf0c4
Zota <zotapianola@gmail.com>
2023-06-27 22:56:17 -0400

    comenzar el viaje

--- app/instrucciones/page.jsx
+++ app/instrucciones/page.jsx
@@ -12,7 +12,7 @@ export default function Instrucciones () {
       <p className='instrucciones-text'>Tienes dos formas de navegar por el contenido: <b>por participantes o por categorías. </b>Recuerda que <b>puedes desplazarte hacia adelante o atrás con las flechas.</b></p>
       <p className='instrucciones-text'><b>Y tranquilx,</b> siempre puedes guiarte por el menú que se encontrará en la parte inferior de la pantalla.</p>
       <div id='btnComenzar'>
-        <Link id='btnComenzarLink' href='/navegacion'>COMENZAR EL VIAJE</Link>
+        <Link id='btnComenzarLink' href='/navegacion'>Comenzar el viaje</Link>
       </div>
     </section>
   )
